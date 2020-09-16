a1287baa5d285fe129b1b88b81a1f5b38233ddf9
titi2194 <diogenes.trejos@gmail.com>
2020-09-16 00:31:29 -0500

    Initial commit

---
+++ app/src/main/java/com/mi3dp/miprimerproyectoandroid_2/MainActivity.java
@@ -0,0 +1,14 @@
+package com.mi3dp.miprimerproyectoandroid_2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
